89edb6dcb50281145e72f5f7014b8161d18750b3
Rubén Noriega Mier <rubennoriegamier@gmail.com>
2024-12-04 19:37:52 +0100

    Refactor day 04 tests

--- tests/day_04.py
+++ tests/day_04.py
@@ -1,27 +1,25 @@
+from pytest import fixture
+
 from aoc.day_04 import part_1, part_2
 
 
-def test_part_1():
-    assert part_1(['MMMSXXMASM',
-                   'MSAMXMSMSA',
-                   'AMXSXMAAMM',
-                   'MSAMASMSMX',
-                   'XMASAMXAMM',
-                   'XXAMMXXAMA',
-                   'SMSMSASXSS',
-                   'SAXAMASAAA',
-                   'MAMMMXMMMM',
-                   'MXMXAXMASX']) == 18
+@fixture
+def sample() -> list[str]:
+    return ['MMMSXXMASM',
+            'MSAMXMSMSA',
+            'AMXSXMAAMM',
+            'MSAMASMSMX',
+            'XMASAMXAMM',
+            'XXAMMXXAMA',
+            'SMSMSASXSS',
+            'SAXAMASAAA',
+            'MAMMMXMMMM',
+            'MXMXAXMASX']
 
 
-def test_part_2():
-    assert part_2(['MMMSXXMASM',
-                   'MSAMXMSMSA',
-                   'AMXSXMAAMM',
-                   'MSAMASMSMX',
-                   'XMASAMXAMM',
-                   'XXAMMXXAMA',
-                   'SMSMSASXSS',
-                   'SAXAMASAAA',
-                   'MAMMMXMMMM',
-                   'MXMXAXMASX']) == 9
+def test_part_1(sample):
+    assert part_1(sample) == 18
+
+
+def test_part_2(sample):
+    assert part_2(sample) == 9
