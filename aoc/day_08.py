bb0d53dfd56835d24875b47ee0690f59b3c17e36
Rubén Noriega Mier <rubennoriegamier@gmail.com>
2024-12-08 11:55:33 +0100

    Replace combinations with permutations

--- aoc/day_08.py
+++ aoc/day_08.py
@@ -1,6 +1,6 @@
 import fileinput
 from collections import defaultdict
-from itertools import combinations, count
+from itertools import count, permutations
 
 
 def main():
@@ -12,21 +12,17 @@ def main():
 
 def part_1(grid: list[str]) -> int:
     antennas = defaultdict(list)
-    antinodes = set()
 
     for y, line in enumerate(grid):
         for x, cell in enumerate(line):
             if cell != '.':
                 antennas[cell].append((y, x))
 
-    for frequency, frequency_antennas in antennas.items():
-        for (y_1, x_1), (y_2, x_2) in combinations(frequency_antennas, 2):
-            if 0 <= (y_3 := y_1 + y_1 - y_2) < len(grid) and 0 <= (x_3 := x_1 + x_1 - x_2) < len(grid[0]):
-                antinodes.add((y_3, x_3))
-            if 0 <= (y_3 := y_2 + y_2 - y_1) < len(grid) and 0 <= (x_3 := x_2 + x_2 - x_1) < len(grid[0]):
-                antinodes.add((y_3, x_3))
-
-    return len(antinodes)
+    # noinspection PyUnboundLocalVariable
+    return len({(y_3, x_3)
+                for frequency, frequency_antennas in antennas.items()
+                for (y_1, x_1), (y_2, x_2) in permutations(frequency_antennas, 2)
+                if 0 <= (y_3 := y_1 + y_1 - y_2) < len(grid) and 0 <= (x_3 := x_1 + x_1 - x_2) < len(grid[0])})
 
 
 def part_2(grid: list[str]) -> int:
@@ -39,15 +35,14 @@ def part_2(grid: list[str]) -> int:
                 antennas[cell].append((y, x))
 
     for frequency, frequency_antennas in antennas.items():
-        for (y_1, x_1), (y_2, x_2) in combinations(frequency_antennas, 2):
+        for (y_1, x_1), (y_2, x_2) in permutations(frequency_antennas, 2):
             antinodes.add((y_1, x_1))
             antinodes.add((y_2, x_2))
-            for y_1_, x_1_, y_2_, x_2_ in [(y_1, x_1, y_2, x_2), (y_2, x_2, y_1, x_1)]:
-                for y_3, x_3 in zip(count(y_1_ + y_1_ - y_2_, y_1_ - y_2_), count(x_1_ + x_1_ - x_2_, x_1_ - x_2_)):
-                    if 0 <= y_3 < len(grid) and 0 <= x_3 < len(grid[0]):
-                        antinodes.add((y_3, x_3))
-                    else:
-                        break
+            for y_3, x_3 in zip(count(y_1 + y_1 - y_2, y_1 - y_2), count(x_1 + x_1 - x_2, x_1 - x_2)):
+                if 0 <= y_3 < len(grid) and 0 <= x_3 < len(grid[0]):
+                    antinodes.add((y_3, x_3))
+                else:
+                    break
 
     return len(antinodes)
 
