7108739ca047681d936529d8c13c50cff8b14b05
Rubén Noriega Mier <rubennoriegamier@gmail.com>
2024-12-20 20:13:55 +0100

    Day 20

---
+++ aoc/day_20.py
@@ -0,0 +1,72 @@
+import fileinput
+
+
+def main():
+    grid = list(map(str.rstrip, fileinput.input()))
+
+    print(part_1(grid, at_least=100))
+    print(part_2(grid, at_least=100, max_ps=20))
+
+
+def grid_with_path(grid: list[str]) -> list[list[int]]:
+    start = -1, -1
+    grid_ = [[-1] * len(grid[0]) for _ in range(len(grid))]
+
+    for y, line in enumerate(grid):
+        for x, tile in enumerate(line):
+            if tile != '#':
+                if tile == 'S':
+                    start = y, x
+                    grid_[y][x] = 1
+                else:
+                    grid_[y][x] = 0
+
+    grid = grid_
+    current = start
+
+    while True:
+        y, x = current
+        for y_, x_ in [(y - 1, x), (y, x + 1), (y + 1, x), (y, x - 1)]:
+            if grid[y_][x_] == 0:
+                grid[y_][x_] = grid[y][x] + 1
+                current = y_, x_
+                break
+        else:
+            break
+
+    return grid
+
+
+def part_1(grid: list[str], *, at_least: int) -> int:
+    grid = grid_with_path(grid)
+
+    return sum(0 <= y_ < len(grid) and 0 <= x_ < len(grid[0]) and grid[y_][x_] - grid[y][x] - 2 >= at_least
+               for y, line in enumerate(grid)
+               for x, tile in enumerate(line)
+               if tile >= 1
+               for y_, x_ in [(y - 2, x), (y, x + 2), (y + 2, x), (y, x - 2)])
+
+
+def part_2(grid: list[str], *, at_least: int, max_ps: int) -> int:
+    grid = grid_with_path(grid)
+    cheats = 0
+
+    for y, line in enumerate(grid):
+        for x, tile in enumerate(line):
+            if tile != -1:
+                for y_ in range(max(y - max_ps, 0), min(y + max_ps + 1, len(grid))):
+                    y_dist = abs(y - y_)
+                    for x_ in range(max(x - max_ps + y_dist, 0), min(x + max_ps - y_dist + 1, len(grid[0]))):
+                        if (tile_ := grid[y_][x_]) != -1:
+                            x_dist = abs(x - x_)
+                            normal_dist = tile_ - tile
+                            shortc_dist = y_dist + x_dist
+
+                            if normal_dist - shortc_dist >= at_least:
+                                cheats += 1
+
+    return cheats
+
+
+if __name__ == '__main__':
+    main()
